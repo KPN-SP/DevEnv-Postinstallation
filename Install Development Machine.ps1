6844320cc04f12f86f8de40d1201651e4c6e10dc
Rob Hofmann <rob.hofmann@HOFMANN.int>
2020-11-05 10:07:39 +0100

    Initial release

---
+++ Install Development Machine.ps1
@@ -0,0 +1,45 @@
+# Browsers
+choco install -y --ignore-checksums googlechrome
+choco install -y --ignore-checksums firefox
+choco install -y --ignore-checksums microsoft-edge
+
+# Communication Tools
+choco install -y --ignore-checksums slack
+choco install -y --ignore-checksums microsoft-teams.install
+
+# Basic tools
+choco install -y --ignore-checksums 7zip.install
+choco install -y --ignore-checksums filezilla
+choco install -y --ignore-checksums powertoys
+
+# Development/management/monitoring tools & dependencies
+choco install -y --ignore-checksums putty.install
+choco install -y --ignore-checksums winscp
+choco install -y --ignore-checksums sysinternals
+choco install -y --ignore-checksums curl
+choco install -y --ignore-checksums vcredist2005
+choco install -y --ignore-checksums vcredist2008
+choco install -y --ignore-checksums vcredist2010
+choco install -y --ignore-checksums vcredist2012
+choco install -y --ignore-checksums vcredist2013
+choco install -y --ignore-checksums vcredist2015
+choco install -y --ignore-checksums vcredist140 #2017
+choco install -y --ignore-checksums rsat
+choco install -y --ignore-checksums git.install
+choco install -y --ignore-checksums gitkraken
+choco install -y --ignore-checksums powershell-core
+choco install -y --ignore-checksums microsoft-windows-terminal
+
+# IDE's
+choco install -y --ignore-checksums vscode
+choco install -y --ignore-checksums azure-data-studio
+choco install -y --ignore-checksums sql-server-management-studio
+choco install -y --ignore-checksums visualstudio2019professional --package-parameters "--passive --locale en-US"
+choco install -y --ignore-checksums visualstudio2019-workload-visualstudioextension
+choco install -y --ignore-checksums visualstudio2019-workload-azure
+choco install -y --ignore-checksums visualstudio2019-workload-manageddesktop
+choco install -y --ignore-checksums visualstudio2019-workload-azurebuildtools
+choco install -y --ignore-checksums visualstudio2019-workload-netcoretools
+choco install -y --ignore-checksums visualstudio2019-workload-netcrossplat
+choco install -y --ignore-checksums visualstudio2019-workload-universal
+choco install -y --ignore-checksums visualstudio2019-workload-netweb
